97c1dcd4571ba36762df0eb77c68195d955924f1
Djinhljnh <125116549+Djinhljnh@users.noreply.github.com>
2024-11-14 09:56:14 +0700

    lollga

--- monkey/Assets/Scripts/ButtonManager.cs
+++ monkey/Assets/Scripts/ButtonManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -10,26 +10,65 @@ public class ButtonManager : MonoBehaviour
 
     public void startGame()
     {
-        SceneManager.LoadScene("NightScene");
-        Time.timeScale = 1;
+        AudioController audioController = FindObjectOfType<AudioController>();
+
+        if (audioController != null)
+        {
+
+            audioController.PlayButtonClickAndChangeScene("NightScene");
+        }
+        else
+        {
+            SceneManager.LoadScene("NightScene");
+        }
+        Time.timeScale = 1; 
     }
     public void returnMenu()
     {
-        SceneManager.LoadScene("Menu");
+        AudioController audioController = FindObjectOfType<AudioController>();
+
+        if (audioController != null)
+        {
+
+            audioController.PlayButtonClickAndChangeScene("Menu");
+        }
+        else
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
         Time.timeScale = 1;
     }
     public void Pause()
     {
+        AudioController audioController = FindObjectOfType<AudioController>();
+
+        if (audioController != null)
+        {
+            audioController.PlayButtonClick();
+        }
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
     }
     public void Resume()
     {
+        AudioController audioController = FindObjectOfType<AudioController>();
+
+        if (audioController != null)
+        {
+            audioController.PlayButtonClick();
+        }
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
     }
     public void Restart()
     {
+        AudioController audioController = FindObjectOfType<AudioController>();
+
+        if (audioController != null)
+        {
+            audioController.PlayButtonClick(); 
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
     }
